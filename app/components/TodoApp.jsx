b87e0c30ee756668f2c9a19e1d97788c19928aa5
BobSears53 <Robert.Sears@LibertyMutual.com>
2017-05-04 12:46:21 -0400

    Add completed and created timestamps

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -18,14 +19,16 @@ var TodoApp = React.createClass({
         TodoAPI.setTodos(this.state.todos);
     },
     handleAddTodo: function (text) {  // WE send this as a Prop to AddTodo
-        // alert('new todo:' + text);
+         //alert('new todo:' + text);
         this.setState({
             todos: [                    // add to the existing array todos
                 ...this.state.todos ,
                 {
                     id: uuid(),  // add the new item
                     text: text,
-                    completed: false
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
                 }
             ]
         });
@@ -34,8 +37,10 @@ var TodoApp = React.createClass({
         var updatedTodos = this.state.todos.map((todo) => {
             if (todo.id === id) {
                 todo.completed = !todo.completed;
+               // alert('completed ' + todo.completed);
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
             }
-
+            //alert('completedAt ' + todo.completedAt);
             return todo;
         });
         this.setState({todos: updatedTodos});
--- app/test/components/TodoApp.test.jsx
+++ app/test/components/TodoApp.test.jsx
@@ -19,13 +19,18 @@ describe('TodoApp', () => {
         todoApp.handleAddTodo(todoText); // add the new item as the first item in the array
 
         expect(todoApp.state.todos[0].text).toBe(todoText);
+        // expect createdAt to be a number
+        console.log('in Test createdAt is ' + todoApp.state.todos[0].createdAt);
+        expect(todoApp.state.todos[0].createdAt).toBeA('number');
     });
 
     it('should toggle completed checkbox when handleToggle called', () => {
         var todoData = {
             id: 11,
             text: 'Test features',
-            completed: false
+            completed: false,
+            createdAt: 0,
+            completedAt: undefined
         };
 
         var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
@@ -38,5 +43,34 @@ describe('TodoApp', () => {
         todoApp.handleToggle(11);
         // Verify that value changed
         expect(todoApp.state.todos[0].completed).toBe(true);
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
+        //Expect completedAt to be a number
+
     });
+
+    it('should toggle todo checkbox from completed  to inCompleted', () => {
+        var todoData = {
+            id: 11,
+            text: 'Test features',
+            completed: true,
+            createdAt: 0,
+            completedAt: 125
+        };
+
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+        todoApp.setState({todos: [todoData]});  // pass the local array to ToDoApp
+
+        // check that todos first item has a completed value of false
+        expect(todoApp.state.todos[0].completed).toBe(true);
+
+        // call handleToggle with 11
+        todoApp.handleToggle(11);
+        // Verify that value changed
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
+        //Expect completedAt to be a number
+
+    });
+
+    //Test that when toggle from true to false, CompletedAt get removed
 });
