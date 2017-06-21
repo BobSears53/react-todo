5fe56a9c25dab6c968615a280fc06fe6aa59551d
BobSears53 <Robert.Sears@LibertyMutual.com>
2017-06-21 14:22:42 -0400

    Refactor todoSearch to work with Redux

--- app/app.jsx
+++ app/app.jsx
@@ -12,13 +12,13 @@ var store = require('configureStore').configure();
 store.subscribe(() => {
     console.log('New state', store.getState());
 });
-
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
+//
 
 
 // Load foundation
+// store.dispatch(actions.addTodo('Clean the yard'));
+// store.dispatch(actions.setSearchText('yard'));
+// store.dispatch(actions.toggleShowCompleted());
 //require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
