832f807b280f6e4ed12d87bd935521dd8e0e97f9
BobSears53 <Robert.Sears@LibertyMutual.com>
2017-07-21 14:04:59 -0400

    Push updated  router/index.jsx file

--- app/router/index.jsx
+++ app/router/index.jsx
@@ -1,3 +1,9 @@
+import React from 'react';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+
+import TodoApp from 'TodoApp';
+import Login from 'Login';
+import firebase from 'app/firebase';
 
 var requireLogin = (nextState, replace, next) => {
     if (!firebase.auth().currentUser) {
@@ -10,6 +16,7 @@ var redirectIfLoggedIn = (nextState, replace, next) => {
     if (firebase.auth().currentUser) {
         replace('/todos');
     }
+
     next();
 };
 
